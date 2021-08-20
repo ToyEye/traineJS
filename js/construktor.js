e128fb93ba63d32512d540cca24f3c397e4d1187
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-08-20 16:08:17 +0300

    salon

--- js/construktor.js
+++ js/construktor.js
@@ -1,19 +1,30 @@
-// const Auto = function (mark, color, year, might) {
-//     this.mark = mark;
-//     this.color = color;
-//     this.year = year;
-//     this.might = might;
-// };
+const Auto = function ({ mark, color, year, might } = {}) {
+    this.mark = mark;
+    this.color = color;
+    this.year = year;
+    this.might = might;
 
-// const audi = new Auto('audi', 'black', 1980, 2.4);
-// console.log(audi);
+   
+};
 
-// const bmw = new Auto('BMW', 'yellow', 2010, 2.4);
-// console.log(bmw);
+Auto.prototype.changeColor = function (newColor) {
+    this.color = newColor;
+}
 
-// const mersedes = new Auto('Merseedes', 'White', 2016, 1.8);
-// console.log(mersedes);
+const audi = new Auto({ mark:'audi', color: 'black',year: 1980,might: 2.4});
+console.log(audi);
 
-// const salon = [];
-// salon.push(audi, bmw, mersedes);
-// console.log(salon);
+const bmw = new Auto({ mark:'BMW',color: 'yellow',year: 2010,might: 2.4});
+console.log(bmw);
+
+const mersedes = new Auto({ mark:'Merseedes',color: 'White',year: 2016, might:1.8 });
+console.log(mersedes);
+
+mersedes.changeColor('Red')
+console.log(mersedes);
+
+
+const salon = [];
+
+salon.push(audi, bmw, mersedes);
+console.log(salon);
